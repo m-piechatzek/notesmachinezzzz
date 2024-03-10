fa4e5ee37f075ba038a0de882ded6f196f310ab5
Monika Piechatzek <monikap7858@gmail.com>
2024-03-10 14:41:19 -0600

    Update cookie-policy.js

--- assets/js/cookie-policy.js
+++ assets/js/cookie-policy.js
@@ -18,16 +18,19 @@ window.onload = function () {
         PopUp('show');
     }, 0);
 }
-const userReject = () => {
+const userReject2 = () => {
   window.onbeforeunload = function (e) {
-      window.onunload = function () {
+      // window.onunload = function () {
         if (window.location.origin != ('https://m-piechatzek.github.io' || 'https://notesmachine.com')) {
                  localStorage.removeItem("popupWasShown");
         }
-      }
-      return undefined;
+      // }
+      // return undefined;
   };
 };
+const userReject = () => {
+  localStorage.removeItem("popupWasShown");
+}
 
 function hideNow(e) {
     if (e.target.id == 'ac-wrapper') document.getElementById('ac-wrapper').style.display = 'none';
